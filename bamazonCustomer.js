2b6fb8cfc2514558e801a9d7207611b0656c8683
leaf-junkie <ragsdale.jar@gmail.com>
2019-03-07 14:06:48 -0500

    cleaning up - moving variables around

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,12 +3,6 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const products = require('./product.js');
 
-// Variable declarations
-const item = response.id;
-const quantity = response.quantity;
-const customerTotal = item * quantity;
-const updatedStock = stock - response.quantity;
-
 // Connect to MySQL
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -17,7 +11,7 @@ const connection = mysql.createConnection({
     password: '12345678',
     database: 'bamazon'
 });
-connection.connect(err => {
+connection.connect(function(err) {
     if(err) throw err;
     console.log(`Connected as ID ${connection.threadId}\n`);
     displayProducts();
@@ -25,10 +19,21 @@ connection.connect(err => {
 });
 
 // Display products from database (id, name, price)
+function displayProducts() {
+    connection.query(`
+    SELECT item_id, product_name, price 
+    FROM products 
+    WHERE ${products.stock_quantity} > 0`, (err, response) => {
+        if(err) throw err;
+        console.log(response);
+        connection.end();
+    });
+}
 displayProducts();
 
 // Start app
 function start() {
+
     inquirer
     .prompt([
         {
@@ -41,11 +46,15 @@ function start() {
             name: 'quantity',
             message: 'How many would you like to purchase?'  
         }
-    ]).then(function() {
+    ]).then(function(err, answer) {
+        const quantity = answer.quantity;
+        let stock = stock_quantity;
+        if(err) throw err;
+        
         // Does store have enough inventory to meet customer's request?
-        if(response.quantity > this.stock) {
+        if(quantity > stock) {
             // NO: Prevent the order from going through
-            console.log(`We apologize for the inconvenience, but only ${this.stock} remain`);
+            console.log(`We apologize for the inconvenience, but only ${stock} remain`);
         // YES: Fulfill the customer's order.
         } else {
             // Update quantity in SQL database
@@ -56,25 +65,22 @@ function start() {
     });
 }
 
-function displayProducts() {
-    connection.query('SELECT * FROM products', (err, response) => {
-        if(err) throw err;
-        console.log(response);
-        connection.end();
-    });
-}
-
 function updateDB() {
+    const item = response.id;
+    const updatedStock = stock - response.quantity;
+
     connection.query(`
     UPDATE products 
     SET stock_quantity = ${updatedStock} 
-    WHERE item_id = ${response.id}
+    WHERE item_id = ${item}
     `);
 }
 
 function displayTotal() {
+    const customerTotal = item * quantity;
+
     for (i = 0; i ; i++) {
-        customerTotal += i;
+        customerTotal;
     }
     console.log(`Your total is ${customerTotal}`);
 }
