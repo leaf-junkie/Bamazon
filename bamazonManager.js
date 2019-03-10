94037d6aa1d27ed9b254dddb30f7def9f052567c
leaf-junkie <ragsdale.jar@gmail.com>
2019-03-10 14:14:15 -0400

    adding more logic to manager application

--- bamazonManager.js
+++ bamazonManager.js
@@ -3,11 +3,13 @@ const inquirer = require('inquirer');
 const Product = require('./product.js');
 const connection = require('./database');
 
-// List a set of menu options:
-//      View Products for Sale
-//      View Low Inventory
-//      Add to Inventory
-//      Add New Product
+connection.connect(function(err) {
+    if(err) console.log(err);
+    console.log(`Connected as ID ${connection.threadId}\n`);
+    menu();
+});
+
+// menu options
 function menu() {
     
     inquirer
@@ -20,26 +22,23 @@ function menu() {
         }
     ]).then(function(response) {
 
-        if(response === 'View Available Products') {
+        if(response.menu === 'View Available Products') {
             // List available items (item IDs, names, prices, quantities)
-            connection.connect(function(err) {
+            displayAll(function(err, products) {
                 if(err) console.log(err);
-                console.log(`Connected as ID ${connection.threadId}\n`);
-                displayAll();
+                console.log(products);
+                menu();
+            });
+                 
+        } else if(response.menu === 'View Low Inventory') {
+            // List all items where inventory < 5
+            displayLowInventory(function(err, products) {
+                if(err) console.log(err);
+                console.log(products);
+                menu();
             });
 
-            function displayAll() {
-                connection.query('SELECT * FROM products'), function(err, products) {
-                    if(err) console.log(err);
-                    console.log(products);
-                    connection.end();
-                }
-            }
-
-        } else if(response === 'View Low Inventory') {
-            // List all items where inventory < 5
-
-        } else if(response === 'Add to Inventory') {
+        } else if(response.menu === 'Add to Inventory') {
             // Display prompt to let manager "add more" of any item currently in the store
             inquirer
             .prompt([
@@ -53,13 +52,27 @@ function menu() {
 
             });
 
-        } else if(response === 'Add New Product') {
-            // Allow manager to add completely new product to store
-            const newProduct = Product(name, department, price, stock);
+        // } else if(response.menu === 'Add New Product') {
+        //     // Allow manager to add completely new product to store
+        //     const newProduct = Product(name, department, price, stock);
 
         } else {
             console.log('You have exited the program.');
         }
     });
 }
-menu();
+
+function displayAll(callback) {
+    connection.query(`
+    SELECT item_id, product_name, price, stock_quantity 
+    FROM products
+    `, callback);
+}
+
+function displayLowInventory(callback) {
+    connection.query(`
+    SELECT item_id, product_name, stock_quantity
+    FROM products
+    WHERE stock_quantity < 5
+    `, callback);
+}
